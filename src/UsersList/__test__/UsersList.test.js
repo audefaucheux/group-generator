d8076abd04f1ec0aed66b3e84d386c3a73b0f8e0
Aude Faucheux <aude53@gmail.com>
2020-04-13 15:59:39 +0100

    correct usersStub import

--- src/UsersList/__test__/UsersList.test.js
+++ src/UsersList/__test__/UsersList.test.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import UsersList from "../UsersList";
-import { UsersStub } from "../../mocks/UsersStub";
+import { usersStub } from "../../mocks/usersStub";
 
 afterEach(cleanup);
 
-test("renders list of users", () => {
+describe("<UsersList />", () => {
   const mockSetUsers = jest.fn();
   const { getByText } = render(
-    <UsersList id={0} users={UsersStub} setUsers={mockSetUsers} />
+    <UsersList id={0} users={usersStub} setUsers={mockSetUsers} />
   );
-  expect(getByText(UsersStub[0])).toBeTruthy();
-  expect(getByText(UsersStub[1])).toBeTruthy();
-  expect(getByText(UsersStub[2])).toBeTruthy();
+
+  it("renders list of users", () => {
+    expect(getByText(usersStub[0])).toBeTruthy();
+    expect(getByText(usersStub[1])).toBeTruthy();
+    expect(getByText(usersStub[2])).toBeTruthy();
+  });
 });
 
 // test("deletes user on click", () => {
